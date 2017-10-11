584d3e9bc7813be8b652c062330cf1654b8ba908
sarath <sarath.o.here@gmail.com>
2017-10-11 12:50:33 +0530

    meassge

--- application/controllers/admin/Configuration.php
+++ application/controllers/admin/Configuration.php
@@ -6,18 +6,34 @@ require_once 'Base_Controller.php';
 class Configuration extends Base_Controller {
 
     public function set_register_fields($action = '', $field_id = '') {
-        $data = array();
-        if ($this->input->post() && $this->validate_register_config()) {//add new fields
+        $res = $this->configuration_model->checkTable('sdfsd');
+        $error_array = $post = array();
+        if ($this->input->post('add_new')) {//add new fields
             $post = $this->input->post();
-            $res = $this->configuration_model->addNewRegistrationField($post);
-            if ($res) {
-                $this->configuration_model->alterTable($post['field_name']);
+            if ($this->validate_field_addition()) {
+                $res = $this->configuration_model->createDbField($post['field_name'], $post['data_types'], $post['data_type_max_size']);
+                if ($res) {
+                    $res=$this->configuration_model->addNewRegistrationField($post);
+                    if($res){
+                        $this->session->unset_userdata('post_data');
+                        $this->loadPage('New Field Created Successfully', 'configuration/set_register_fields', True);
+                    }else{
+                        $this->loadPage('New Field Creation Failed', 'configuration/set_register_fields', FALSE);
+                    }
+                } else {
+                    $this->loadPage('New Field Creation Failed', 'configuration/set_register_fields', FALSE);
+                }
             } else {
-                
+                $this->session->set_userdata('post_data', $post);
+                $error_array = $this->form_validation->error_array();
+                //print_r($error_array);die();
+                $this->loadPage('Validation Error', 'configuration/set_register_fields', FALSE);
             }
         }
-        $error_array = $this->form_validation->error_array();
 
+        $post_data=$this->session->userdata('post_data');
+        
+        
         $edit_status = FALSE;
         $editable_fields = array();
         if ($field_id && $action) {//update new fields
@@ -36,18 +52,46 @@ class Configuration extends Base_Controller {
                 $msg = 'This Field Can not be Edited';
             }
         }
-        $fields = array();//$this->configuration_model->getAllRegFields();
-
+        $fields = array(); //$this->configuration_model->getAllRegFields();
+//print_r($post_data);die();
         $this->setData('base_path', base_url());
         $this->setData('error_array', $error_array);
         $this->setData('fields', $fields);
         $this->setData('edit_status', $edit_status);
         $this->setData('editable_fields', $editable_fields);
+        $this->setData('post_data', $post_data);
         $this->loadView();
     }
 
+    function validate_field_addition() {
+        $this->form_validation->set_rules('field_name', 'field_name', 'required|callback_validate_field|trim');
+        $this->form_validation->set_rules('required_status', 'required_status', 'required');
+        $this->form_validation->set_rules('register_step', 'register_step', 'required');
+        $this->form_validation->set_rules('order', 'order', 'required');
+        $this->form_validation->set_rules('unique_status', 'unique_status', 'required');
+        $this->form_validation->set_rules('data_types', 'data_types', 'required');
+        if ($this->input->post('data_types') != 'double' && $this->input->post('data_types') != 'text') {
+            $this->form_validation->set_rules('data_type_max_size', 'data_type_max_size', 'required');
+        }
+
+        $this->form_validation->set_rules('field_type', 'field_type', 'required');
+        if ($this->input->post('field_type') == 'radio') {
+            $this->form_validation->set_rules('radio_value1', 'radio_value1', 'required');
+            $this->form_validation->set_rules('radio_value2', 'radio_value2', 'required');
+        }
+        if ($this->input->post('field_type') == 'select_box') {
+            $this->form_validation->set_rules('select_option1', 'select_option1', 'required');
+            $this->form_validation->set_rules('select_option2', 'select_option2', 'required');
+            $this->form_validation->set_rules('select_option3', 'select_option3', 'required');
+            $this->form_validation->set_rules('select_option4', 'select_option4', 'required');
+        }
+        $this->form_validation->set_error_delimiters('<li>', '</li>');
+        $validation = $this->form_validation->run();
+        return $validation;
+    }
+
     function validate_register_config() {
-        $this->form_validation->set_rules('field_name', 'Field Name', 'required|callback_validate_field|trim');
+        $this->form_validation->set_rules('field_name', 'Field Name', 'required|callback_validate_field');
         $this->form_validation->set_rules('required_field', 'Req Field', 'required');
         $this->form_validation->set_rules('input_type', 'input_type', 'required');
         $this->form_validation->set_rules('field_type', 'field_type', 'required');
@@ -61,9 +105,9 @@ class Configuration extends Base_Controller {
     }
 
     function validate_field($field) {
-        $flag = true;
-        if ($this->configuration_model->checkField($field)) {
-            $flag = false;
+        $flag = false;
+        if ($this->configuration_model->checkField($field) || $this->configuration_model->checkTable($field)) {
+            $flag = true;
         }
         return $flag;
     }
--- application/models/Configuration_model.php
+++ application/models/Configuration_model.php
@@ -1,5 +1,60 @@
 <?php
 
 class Configuration_model extends CI_Model {
+
+    function checkField($status) {
+        $numrows = $this->db->select('field_name')
+                ->from("register_fields")
+                ->where('status !=', "deleted")
+                ->count_all_results();
+        return $numrows;
+    }
     
+    function checkTable($field) {
+        $res=0;
+        $columns=$this->db->list_fields('user_details');
+        foreach ($columns AS $key => $value) {
+            if($value==$field){
+                $res=1;
+            }
+        }
+        return $res;
+    }
+
+    function createDbField($field_name, $data_types, $constraint) {
+        $this->load->dbforge();
+        if ($data_types == "text" || $data_types == "double") {
+            $fields = array(
+                $field_name => array('type' => $data_types)
+            );
+        } else {
+            $fields = array(
+                $field_name => array('type' => $data_types,
+                    'constraint' => $constraint,
+                    'null' => true
+                )
+            );
+        }
+        return $this->dbforge->add_column('user_details', $fields);
+    }
+    
+    function addNewRegistrationField($data) {
+        return $this->db->set('field_name', $data['field_name'])
+                        ->set('required_status', $data['required_status'])
+                        ->set('unique_status', $data['unique_status'])
+                        ->set('register_step', $data['register_step'])
+                        ->set('order', $data['order'])
+                        ->set('default_value', $data['default_value'])
+                        ->set('data_types', $data['data_types'])
+                        ->set('data_type_max_size', $data['data_type_max_size'])
+                        ->set('field_type', $data['field_type'])
+                        ->set('radio_value1', $data['radio_value1'])
+                        ->set('radio_value2', $data['radio_value2'])
+                        ->set('select_option1', $data['select_option1'])
+                        ->set('select_option2', $data['select_option2'])
+                        ->set('select_option3', $data['select_option3'])
+                        ->set('select_option4', $data['select_option4'])
+                        ->set('status', "active")
+                        ->insert('register_fields');
+    }
 }
