225d653a1d08566901481d1ce0ad6420d1723a4e
sarath <sarath.o.here@gmail.com>
2017-10-11 20:16:47 +0530

    All Queries

---
+++ queries.php
@@ -0,0 +1,3 @@
+<?php
+// ALTER TABLE `mlm_activity` ADD `data` TEXT NOT NULL ;
+//INSERT INTO `lead_mlm`.`mlm_menus` (`id`, `name`, `link`, `root_id`, `icon`, `admin_permission`, `user_permission`, `employee_permission`, `order`, `target`, `status`, `lock`) VALUES (NULL, 'Register Field Configuration', 'configuration/set_register_fields', '#', 'fa-bolt', '1', '0', '1', '5', 'null', '1', '0');
