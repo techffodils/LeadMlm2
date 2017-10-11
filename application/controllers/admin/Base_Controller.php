804cedf0937e63a9c50cb5f7ca2d344eeab61cce
sarath <sarath.o.here@gmail.com>
2017-10-11 18:23:19 +0530

    meassge

--- application/controllers/admin/Base_Controller.php
+++ application/controllers/admin/Base_Controller.php
@@ -1,23 +1,15 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 class Base_Controller extends Core_Base_Controller {
 
     function __construct() {
 
         parent::__construct();
 
-        //$this->main->load_model();
-
         if (!in_array($this->main->get_controller(), NO_LOGIN_PAGES)) {
-            $is_logged_in = $this->checkLogged('admin');
+            $this->checkLogged('admin');
         } else {
-            if($this->main->get_controller()=="login" && $this->main->get_method() == "index"){
+            if ($this->main->get_controller() == "login" && $this->main->get_method() == "index") {
                 $this->checkPages();
             }
         }
--- application/controllers/user/Base_Controller.php
+++ application/controllers/user/Base_Controller.php
@@ -1,22 +1,15 @@
 <?php
 
-/**
- * @author Techffodils LLP
- * @date 2017-10-10
- * 
- */
-
 class Base_Controller extends Core_Base_Controller {
 
     function __construct() {
 
         parent::__construct();
-        $is_logged_in = false;
 
         if (!in_array($this->main->get_controller(), NO_LOGIN_PAGES)) {
-            $is_logged_in = $this->checkLogged('user');
+            $this->checkLogged('user');
         } else {
-            if($this->main->get_controller()=="login" && $this->main->get_method() == "index"){
+            if ($this->main->get_controller() == "login" && $this->main->get_method() == "index") {
                 $this->checkPages();
             }
         }
