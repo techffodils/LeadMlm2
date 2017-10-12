69498f99a18c1b1ff8f7552eac2b7a66e5e926c0
techffodils <32633457+techffodils@users.noreply.github.com>
2017-10-12 12:30:54 +0530

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','twig', 'session','main','form_validation','encrypt','email');
+$autoload['libraries'] = array('database','twig', 'session','main','form_validation','encrypt','email','dbvars');
 
 /*
 | -------------------------------------------------------------------
