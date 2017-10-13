049ebf6e07b186d441273b983372232d07019991
techffodils <32633457+techffodils@users.noreply.github.com>
2017-10-13 21:57:19 +0530

    Update Home.php

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -11,11 +11,13 @@ class Home extends Base_Controller{
 	function index(){
 		
 		$title="Home";
-	    $this->setData('title',$title.'|'.$this->main->get_controller() .'::');
+	         $this->setData('title',$title.'|'.$this->main->get_controller() .'::');
 	
-		$this->setData('header','Home');
+		$this->BREADCRUM_DATA=array('page_title'=>$title,'page_sub_title'=>$title,'page_header'=>$title,'page_header_small'=>$title);
 		
-			$this->loadView();
+		$this->set_header_lang();
+		
+	      $this->loadView();
 	}
 	
 	
