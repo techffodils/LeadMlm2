2936df25a218e22ddc5d63124772cc3c8dea2d76
sarath <sarath.o.here@gmail.com>
2017-10-13 11:13:07 +0530

    meassge

--- queries.php
+++ queries.php
@@ -5,7 +5,16 @@
 //ALTER TABLE `mlm_cron_job` ADD `file_status` VARCHAR( 30 ) NOT NULL DEFAULT 'active';
 //ALTER TABLE `mlm_cron_job` ADD `done_by` VARCHAR( 30 ) NOT NULL DEFAULT 'Cron Job';
 //ALTER TABLE `mlm_payment_methods` CHANGE `payment_methode` `payment_method` VARCHAR( 30 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ;
-//
-//
-//
+//INSERT INTO `lead_mlm`.`mlm_menus` (`id`, `name`, `link`, `root_id`, `icon`, `admin_permission`, `user_permission`, `employee_permission`, `order`, `target`, `status`, `lock`) VALUES (NULL, 'Plan Settings', 'configuration/plan_settings', '#', 'fa fa-barcode', '1', '0', '1', '5', NULL, '1', '0');
 //INSERT INTO `lead_mlm`.`mlm_menus` (`id`, `name`, `link`, `root_id`, `icon`, `admin_permission`, `user_permission`, `employee_permission`, `order`, `target`, `status`, `lock`) VALUES (NULL, 'Database Backup', 'backup', '#', 'fa-database', '1', '0', '1', '5', 'null', '1', '0');
+
+
+
+
+
+
+
+
+
+
+
