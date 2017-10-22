633f5c6c6f8df08a967081e5dcdb3cf3eb5806ba
sarath <sarath@gmail.com>
2017-10-22 21:10:44 +0530

    All Queries

--- application/controllers/admin/Profile.php
+++ application/controllers/admin/Profile.php
@@ -38,6 +38,26 @@ class Profile extends Base_Controller {
                 $this->loadPage('Profile Pic Updated', 'profile/index', TRUE);
             }
         }
+        
+        if ($this->input->post('cover_update')) {
+            $config['upload_path'] = FCPATH . 'assets/images/users/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size'] = 10000;
+            $config['max_width'] = 2048;
+            $config['max_height'] = 2048;
+            $new_name = 'dp_'.time();
+            $config['file_name'] = $new_name;
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('cover_pic')) {
+                $error = array('error' => $this->upload->display_errors());
+                $this->loadPage('Profile Pic Updation Failed', 'profile/index', FALSE);
+            } else {
+                $data = array('upload_data' => $this->upload->data());
+                $this->profile_model->updateUserCover($user_id,$data);
+                $this->loadPage('Profile Pic Updated', 'profile/index', TRUE);
+            }
+        }
+        
 
         $user_details = $this->profile_model->getUserDetails($user_id);
 
