23b4a815d8d58d0c1b700173175aeec1e6f879b4
sarath <sarath@gmail.com>
2017-10-23 22:24:47 +0530

    All Queries

--- application/controllers/user/Home.php
+++ application/controllers/user/Home.php
@@ -16,11 +16,7 @@ class Home extends Base_Controller{
 	*/
 	function index(){
 		$title="Home";
-		$this->setData('title',$title.'|'.$this->main->get_controller() .'::');
-		
-		$this->BREADCRUM_DATA=array('page_title'=>$title,'page_sub_title'=>$title,'page_header'=>$title,'page_header_small'=>$title);
-		
-		$this->set_header_lang();
+		$this->setData('title',$title);
 		
 		$this->loadView();
 	}
