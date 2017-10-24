4df95629daf9f642f8e02e3e92812a6c58689eeb
sarath <sarath@gmail.com>
2017-10-24 16:46:49 +0530

    All Queries

--- application/models/Wallet_model.php
+++ application/models/Wallet_model.php
@@ -20,11 +20,13 @@ class Wallet_model extends CI_Model {
         $res = $this->db->select("to_user,amount,transfer_type,date")
                 ->from("wallet_transfer")
                 ->where("from_user", $user_id)
+                ->where("transfer_type","transfer_by_user")
+                ->order_by('id','desc')
                 ->get();
         $i = 0;
         foreach ($res->result() as $row) {
             $data[$i]['slno'] = $i + 1;
-            $data[$i]['to_user'] = $row->to_user;
+            $data[$i]['to_user'] = $this->helper_model->IdToUserName($row->to_user);
             $data[$i]['full_name'] =  $this->helper_model->getUserFullName($row->to_user);
             $data[$i]['transfer_type'] = $row->transfer_type;
             $data[$i]['date'] = $row->date;
