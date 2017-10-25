804f891661a6154d9995fc0f3f250548599fd65d
techffodils <32633457+techffodils@users.noreply.github.com>
2017-10-25 13:12:23 +0530

    Add files via upload

--- application/language/english/user/member_lang.php
+++ application/language/english/user/member_lang.php
@@ -1,8 +1,28 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$lang['account_settings'] = 'Account Settings';
+$lang['change_password'] = 'Change Password';
+$lang['change_user_name'] = 'Chnage User Name';
+$lang['change_transation_password'] = 'Change Transation Password';
+$lang['change_user_name'] = 'Change User Name';
+$lang['re_enter_user_name'] = 'Re-Enter  User Name';
+$lang['new_user_name'] = 'New User Name';
+$lang['current_user_name'] = 'Current User Name';
+$lang['change_your_username'] = 'Change Your UserName';
+$lang['change_password'] = 'Change Password';
+$lang['re_enter_password'] = 'Re-Enter  Password';
+$lang['new_password'] = 'New Password';
+$lang['current_password'] = 'Current Password';
+$lang['change_your_password'] = 'Change Your Password';
+$lang['change_transation_password'] = 'Change Transation Password';
+$lang['re_enter_password'] = 'Re-Enter  Password';
+$lang['new_password'] = 'New Password';
+$lang['Current Password'] = 'current_password';
+$lang['change_transation_password'] = 'Change Transation Password';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
 
