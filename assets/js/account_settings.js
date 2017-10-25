fbe0958157a0f73f67343c8f0f5b7623b90b98bd
techffodils <32633457+techffodils@users.noreply.github.com>
2017-10-25 12:01:24 +0530

    Add files via upload

--- assets/js/account_settings.js
+++ assets/js/account_settings.js
@@ -220,6 +220,8 @@ function toggle_password5(target){
     });
         
     }
+    
+    
     function changetransation() {
         
          var tran_user_name = $('#tran_user_name').val();
@@ -246,6 +248,125 @@ function toggle_password5(target){
                     document.getElementById('tran_user_name').value="";
                     document.getElementById('tran_pass_password').value="";
                     document.getElementById('tran_pass_re_enter_password').value="";
+                
+
+            } else {
+                var flag = "error";
+                var msg = result;
+                var title = "Failed";
+                show_notification(flag, title, msg);
+            }
+        }
+    });
+        
+    }
+    
+    //========================= user side ============================//
+    
+    function changetransationuser() {
+        
+         var tran_current_password = $('#tran_current_password').val();
+         var tran_pass_password = $('#tran_pass_password').val();
+         var tran_pass_re_enter_password = $('#tran_pass_re_enter_password').val();
+         
+       $.blockUI({
+        message: '<i class="fa fa-spinner fa-spin"></i> Please Wait...'
+    });
+
+    $.ajax({
+       
+       
+        url: "user/member/update_transation",
+        data: {tran_current_password: tran_current_password, tran_pass_password: tran_pass_password,tran_pass_re_enter_password:tran_pass_re_enter_password},
+        success: function (result) {
+            if (result == "yes") {
+                
+                    var msg = "Sucessfully Changed Transation Password";
+                    var flag = "success";
+                    var title = "Success";
+                    show_notification(flag, title, msg);
+                    
+                    document.getElementById('tran_current_password').value="";
+                    document.getElementById('tran_pass_password').value="";
+                    document.getElementById('tran_pass_re_enter_password').value="";
+                
+
+            } else {
+                var flag = "error";
+                var msg = result;
+                var title = "Failed";
+                show_notification(flag, title, msg);
+            }
+        }
+    });
+        
+    }
+    
+     function changeusernameuser() {
+        
+         var uname_current_username = $('#uname_current_username').val();
+         var uname_new_username = $('#uname_new_username').val();
+         var uname_re_entry_username = $('#uname_re_entry_username').val();
+         
+       $.blockUI({
+        message: '<i class="fa fa-spinner fa-spin"></i> Please Wait...'
+    });
+
+    $.ajax({
+       
+       
+        url: "user/member/update_username",
+        data: {uname_current_username: uname_current_username, uname_new_username: uname_new_username,uname_re_entry_username:uname_re_entry_username},
+        success: function (result) {
+            if (result == "yes") {
+                
+                    var msg = "Sucessfully Changed User Name";
+                    var flag = "success";
+                    var title = "Success";
+                    show_notification(flag, title, msg);
+                    
+                    document.getElementById('uname_current_username').value="";
+                    document.getElementById('uname_new_username').value="";
+                    document.getElementById('uname_re_entry_username').value="";
+                
+
+            } else {
+                var flag = "error";
+                var msg = result;
+                var title = "Failed";
+                show_notification(flag, title, msg);
+            }
+        }
+    });
+        
+    }
+    
+     function changepassworduser() {
+        
+         var pass_current_password = $('#pass_current_password').val();
+         var pass_password = $('#pass_password').val();
+         var pass_re_enter_password = $('#pass_re_enter_password').val();
+         
+       $.blockUI({
+        message: '<i class="fa fa-spinner fa-spin"></i> Please Wait...'
+    });
+
+    $.ajax({
+       
+       
+        url: "user/member/update_password",
+        data: {pass_current_password: pass_current_password, pass_password: pass_password,pass_re_enter_password:pass_re_enter_password},
+        success: function (result) {
+            if (result == "yes") {
+                
+                    var msg = "Sucessfully Changed Password";
+                    var flag = "success";
+                    var title = "Success";
+                    show_notification(flag, title, msg);
+                    
+                    document.getElementById('pass_current_password').value="";
+                    document.getElementById('pass_re_enter_password').value="";
+                    document.getElementById('pass_password').value="";
                 
 
             } else {
