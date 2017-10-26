971ffdb022dcd7d2a25a6806df9867a7b3fbc89b
sarath <sarath@gmail.com>
2017-10-26 12:11:22 +0530

    Merge origin/master

---
+++ application/language/english/register_lang.php
@@ -0,0 +1,62 @@
+<?php
+
+
+$lang['single_step_registration'] = 'Single Step Registration';
+$lang['sponser_name'] = 'Sponsor Name';
+$lang['username'] = 'Username';
+$lang['email'] = 'E-Mail';
+$lang['password'] = 'Password';
+$lang['confirm_password'] = 'Confirm Password';
+$lang['first_name'] = 'First Name';
+$lang['agree_to_the_terms_of_service'] = $lang['agree'] ='I agree to the Terms of Service and Privacy Policy';
+$lang['register'] = 'Register';
+$lang['agree'] = 'Agree';
+$lang['phone_number'] = 'Phone Number';
+$lang['gender'] = 'Gender';
+$lang['address'] = 'Address';
+$lang['country'] = 'Country';
+$lang['zip_code'] = 'Zip Code';
+
+$lang['next'] = 'Next';
+$lang['back'] = 'Back';
+$lang['mm'] = 'MM';
+$lang['Expiration(MM/YYYY)'] = 'Expiration(MM/YYYY)';
+$lang['cvc'] = 'CVC';
+$lang['card_number'] = 'Card Number';
+$lang['card_name'] = 'Card Holder Name';
+
+$lang['city'] = 'City';
+$lang['select_a_state'] = 'Select a state';
+$lang['state'] = 'State';
+$lang['Country'] = 'Country';
+$lang['select_a_country'] = 'Select a country';
+$lang['address'] =$lang['address_1'] = 'Address';
+$lang['Male'] =$lang['m'] =  'Male';
+$lang['Female'] =$lang['f'] = 'Female';
+$lang['last_name'] = 'Last Name';
+
+$lang['multiple_step_registration'] = 'Multiple Step Registration';
+
+$lang['personal_info'] = 'Personal Information';
+$lang['sponsor_info'] = 'Sponsor Information';
+$lang['payment_options'] = 'Payment Options';
+$lang['billing_info'] = 'Billing Information';
+
+$lang['step-1'] = 'Step-1';
+$lang['step-2'] = 'Step-2';
+$lang['step-3'] = 'Step-3';
+$lang['step-4'] = 'Step-4';
+
+$lang['register_success'] = 'User Registered Successfully';
+$lang['register_failed'] = 'Failed To Register';
+$lang['advanced_registration'] = 'Advanced Registration';
+
+$lang['left'] = 'Left';
+$lang['right'] = 'Right';
+$lang['register_leg'] = 'Register Leg';
+$lang['select_a_register_leg'] = ' Select Register Leg';
+$lang['select_a_country'] = ' Select A Country';
+$lang['country'] = 'Country';
+$lang['select_a'] = 'Select A';
+$lang['register_leg '] = 'Register Leg ';
+
