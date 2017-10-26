dec718077cc63cb630cbff70db692dcbc7ca676c
sarath <sarath@gmail.com>
2017-10-26 12:12:40 +0530

    Merge origin/master

--- application/language/english/common_lang.php
+++ application/language/english/common_lang.php
@@ -1,5 +1,4 @@
 <?php
-
 //Menu Start
 $lang['menu_name_1'] = 'Dashboard';
 $lang['menu_name_2'] = 'Tree';
@@ -153,12 +152,10 @@ $lang['UP1275']	= '(UTC +12:45) Chatham Islands Standard Time';
 $lang['UP13']	= '(UTC +13:00) Samoa Time Zone, Phoenix Islands Time, Tonga';
 $lang['UP14']	= '(UTC +14:00) Line Islands';
 
-
 $lang['pagination_first_link'] = '&lsaquo; First';
 $lang['pagination_next_link'] = '&gt;';
 $lang['pagination_prev_link'] = '&lt;';
 $lang['pagination_last_link'] = 'Last &rsaquo;';
-
 
 $lang['collapse'] = 'Collapse';
 $lang['refresh'] = 'Refresh';
@@ -166,55 +163,6 @@ $lang['config'] = 'Configurations';
 $lang['full_screen'] = 'Fullscreen';
 $lang['sign_in_to_ur_account'] = 'Sign in to your account';
 $lang['form_error_msg'] = 'You have some form errors. Please check below.';
-$lang['single_step_registration'] = 'Single Step Registration';
-$lang['sponser_name'] = 'Sponsor Name';
-$lang['username'] = 'Username';
-$lang['email'] = 'E-Mail';
-$lang['password'] = 'Password';
-$lang['confirm_password'] = 'Confirm Password';
-$lang['first_name'] = 'First Name';
-$lang['agree_to_the_terms_of_service'] = $lang['agree'] ='I agree to the Terms of Service and Privacy Policy';
-$lang['register'] = 'Register';
-$lang['agree'] = 'Agree';
-$lang['phone_number'] = 'Phone Number';
-$lang['gender'] = 'Gender';
-$lang['address'] = 'Address';
-$lang['country'] = 'Country';
-$lang['zip_code'] = 'Zip Code';
-
-$lang['next'] = 'Next';
-$lang['back'] = 'Back';
-$lang['mm'] = 'MM';
-$lang['Expiration(MM/YYYY)'] = 'Expiration(MM/YYYY)';
-$lang['cvc'] = 'CVC';
-$lang['card_number'] = 'Card Number';
-$lang['card_name'] = 'Card Holder Name';
-
-$lang['city'] = 'City';
-$lang['select_a_state'] = 'Select a state';
-$lang['state'] = 'State';
-$lang['Country'] = 'Country';
-$lang['select_a_country'] = 'Select a country';
-$lang['address'] =$lang['address_1'] = 'Address';
-$lang['Male'] =$lang['m'] =  'Male';
-$lang['Female'] =$lang['f'] = 'Female';
-$lang['last_name'] = 'Last Name';
-
-$lang['multiple_step_registration'] = 'Multiple Step Registration';
-
-$lang['personal_info'] = 'Personal Information';
-$lang['sponsor_info'] = 'Sponsor Information';
-$lang['payment_options'] = 'Payment Options';
-$lang['billing_info'] = 'Billing Information';
-
-$lang['step-1'] = 'Step-1';
-$lang['step-2'] = 'Step-2';
-$lang['step-3'] = 'Step-3';
-$lang['step-4'] = 'Step-4';
-
-$lang['register_success'] = 'User Registered Successfully';
-$lang['register_failed'] = 'Failed To Register';
-$lang['advanced_registration'] = 'Advanced Registration';
 
 $lang['remove'] = 'Remove';
 $lang['change'] = 'Change';
@@ -225,14 +173,7 @@ $lang['credited_by_admin'] = 'Fund Credited By Admin';
 $lang['debited_by_admin'] = 'Fund Debited By Admin';
 $lang['transfer_by_user'] = 'Fund Transfered';
 
-$lang['left'] = 'Left';
-$lang['right'] = 'Right';
-$lang['register_leg'] = 'Register Leg';
-$lang['select_a_register_leg'] = ' Select Register Leg';
-$lang['select_a_country'] = ' Select A Country';
-$lang['country'] = 'Country';
-$lang['select_a'] = 'Select A';
-$lang['register_leg '] = 'Register Leg ';
+
 
 
 
