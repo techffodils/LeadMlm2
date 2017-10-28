22126580673999e33eabf5f5d053c97d94102b3d
sreejil <sreejilvk@gmail.com>
2017-10-28 07:17:25 -0700

    Merge origin/master

---
+++ application/language/english/user/configuration_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$lang['add_kyc_details'] = "Add Kyc Details";
+$lang['form_error_messages'] = "You have some form errors. Please check below.";
+$lang['form_success_messages'] = "Your form validation is successful!";
+$lang['bank_details'] = "Bank Details";
+$lang['id_proof'] = "Identity Proof Details";
+$lang['bank_name'] = "Bank Name";
+$lang['bank_account_no'] = "Bank Account Number";
+$lang['bank_branch'] = "Bank Branch";
+$lang['upload_proof'] = "Upload Bank Proof";
+$lang['bank_ifc_code'] = "IFC/SWIFT Code";
+$lang['select_file'] = "Select file";
+$lang['change'] = "Change";
+$lang['remove'] = "Remove";
+$lang['name_as_per_proof'] = "Name as per Proof";
+$lang['id_number'] = "Id Number";
+$lang['please_select_allowcd_iamges'] = "Please select images";
+$lang['please_select_allocated_iamges'] = "Please select allocated images jpeg/png/gif";
+$lang['successfully_upload_kyc_details'] = "Sucessfully Upload Kyc Details";
+$lang['error_upload_kyc_details'] = "Error while upload Kyc Details";
