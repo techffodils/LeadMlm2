566a38ecf28b8cf2e024c0adc32ab8bc618b6910
sreejil <sreejilvk@gmail.com>
2017-11-01 00:14:59 -0700

    Merge origin/master

--- application/models/Configuration_model.php
+++ application/models/Configuration_model.php
@@ -95,7 +95,7 @@ class Configuration_model extends CI_Model {
     function changeFieldStatus($id, $status) {
         return $this->db->set('status ', "$status")
                         ->where('editable_status ', 1)
-                         ->where('id ', $id)
+                        ->where('id ', $id)
                         ->update('register_fields');
     }
 
@@ -104,7 +104,7 @@ class Configuration_model extends CI_Model {
         $res = $this->db->select("*")
                 ->from("register_fields")
                 ->where("status !=", 'deleted')
-                ->where("id",$id)
+                ->where("id", $id)
                 ->get();
         foreach ($res->result() as $row) {
             $data['id'] = $row->id;
@@ -173,7 +173,7 @@ class Configuration_model extends CI_Model {
         }
         return $this->dbforge->modify_column('user_details', $fields);
     }
-    
+
     function updateRegistrationField($data) {
         return $this->db->set('field_name', $data['field_name'])
                         ->set('field_name_en', $data['field_name_en'])
@@ -192,9 +192,9 @@ class Configuration_model extends CI_Model {
                         ->set('select_option3', $data['select_option3'])
                         ->set('select_option4', $data['select_option4'])
                         ->where('id', $data['edited_field'])
-                        ->update('register_fields'); 
+                        ->update('register_fields');
     }
-    
+
     function getAllPaymentMethods() {
         $data = array();
         $res = $this->db->select("id,code,payment_method,status")
@@ -210,7 +210,7 @@ class Configuration_model extends CI_Model {
         }
         return $data;
     }
-    
+
     function changePaymentStatus($code, $status) {
         return $this->db->set('status ', "$status")
                         ->where('code ', $code)
@@ -229,20 +229,20 @@ class Configuration_model extends CI_Model {
             $data[$i]['name'] = $row->currency_name;
             $data[$i]['status'] = $row->status;
             $data[$i]['icon'] = $row->icon;
-            $data[$i]['currency_ratio'] = (float)$row->currency_ratio;
+            $data[$i]['currency_ratio'] = (float) $row->currency_ratio;
             $i++;
         }
         return $data;
     }
-    
+
     function changeCurrencyStatus($id, $status) {
         return $this->db->set('status ', "$status")
-                         ->where('id ', $id)
+                        ->where('id ', $id)
                         ->update('currencies');
     }
 
     function getLanguages() {
-        
+
         $data = array();
         $res = $this->db->select("id,lang_code,lang_name,status")
                 ->from("languages")
@@ -257,13 +257,13 @@ class Configuration_model extends CI_Model {
         }
         return $data;
     }
-    
+
     function changeLanguageStatus($id, $status) {
         return $this->db->set('status ', "$status")
-                         ->where('id ', $id)
+                        ->where('id ', $id)
                         ->update('languages');
     }
-    
+
     public function getCurrencySymbol($code) {
         $icon = '';
         $query = $this->db->select('icon')
@@ -275,15 +275,16 @@ class Configuration_model extends CI_Model {
         }
         return $icon;
     }
-    
-    function addNewLanguageField($user_id,$field_name,$in_english) {
+
+    function addNewLanguageField($user_id, $field_name, $in_english) {
         return $this->db->set('mlm_user_id', $user_id)
                         ->set('field_name', $field_name)
                         ->set('in_english', $in_english)
                         ->set('date', date("Y-m-d H:i:s"))
                         ->insert('language_conversion');
     }
-       /**
+
+    /**
      * 
      * 
      * For insert images upload
@@ -317,5 +318,185 @@ class Configuration_model extends CI_Model {
             return TRUE;
         }
     }
-    
+
+    /**
+     * For Getting All Kyc Request Details
+     * 
+     * @author techffodils
+     * 
+     * @date 2017-10-27
+     * 
+     * 
+     */
+    function getAllKycDetails() {
+        $query = $this->db->select("kyc_details.user_id,user.user_name,user.email,user.date")
+                        ->from('kyc_details')
+                        ->where("(id_proof_status ='pending' OR bank_proof_status ='pending')")
+                        ->join('user', 'kyc_details.user_id=user.mlm_user_id')->get();
+        $i = 0;
+        $data = [];
+        foreach ($query->result_array() as $row) {
+            $data[$i]['user_name'] = $row['user_name'];
+            $data[$i]['user_id'] = $row['user_id'];
+            $data[$i]['email'] = $row['email'];
+            $data[$i]['date'] = $row['date'];
+            $i++;
+        }
+        return $data;
+    }
+
+    function gettingUserKycDetails($user_id) {
+        $query = $this->db->select('*')
+                ->from('kyc_details')
+                ->where('user_id', $user_id)
+                ->get();
+        $data = $bank_status = $id_proof_status = '';
+        foreach ($query->result_array() as $row) {
+            $bank_status = $row['bank_proof_status'];
+            $id_proof_status = $row['id_proof_status'];
+            $data .= '<div class="row">';
+            $data .= '<input type="hidden" name="user_id" id="user_id" value="' . $row['user_id'] . '">';
+            $data .= '<div class="col-md-6">';
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('bank_name') . '</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="bank_name" id="bank_name" value="' . $row['bank_name'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('bank_branch') . ':</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="bank_branch" id="bank_branch" value="' . $row['bank_branch'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('bank_account_number') . ':</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="bank_account_number" id="bank_account_number" value="' . $row['bank_account_number'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('bank_ifsc_code') . ':</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="bank_ifsc_code" id="bank_ifsc_code" value="' . $row['bank_ifsc_code'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('bank_proof') . ':</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<img width="150" height="150" src="' . BASE_PATH . 'assets/images/bank_details/' . $row['bank_proof'] . '"/>';
+
+            $data .= '</div>';
+            $data .= '</div>';
+            $data .= '<div class="form-group">';
+            $data .= '<div class="input-group">';
+            if ($bank_status == 'accept') {
+                $data .= '<input type="hidden" name="bank_proof_status"   id="bank_accept" value="accept" onclick="setIdProofAccept(' . $row['user_id'] . ', 1)" > <span style="color:green" class="fa fa-check">  ' . lang('accepted') . '</span>&nbsp;&nbsp;';
+                $data .= '<input type="radio" name="bank_proof_status"  id="bank_reject" value="reject" onclick="setIdProofReject(' . $row['user_id'] . ', 1)"  > ' . lang('reject') . '';
+            } elseif ($bank_status == 'reject') {
+                $data .= '<input type="radio" name="bank_proof_status"   id="bank_accept" value="accept" onclick="setIdProofAccept(' . $row['user_id'] . ', 1)" > ' . lang('accept') . '</span>&nbsp;&nbsp;';
+                $data .= '<input type="hidden" name="bank_proof_status"  id="bank_reject" value="reject" onclick="setIdProofReject(' . $row['user_id'] . ', 1)"  > <span style="color:red" class="fa fa-trash">  ' . lang('rejected') . '</span>';
+            } else {
+                $data .= '<input type="radio" name="bank_proof_status"   id="bank_accept" value="accept" onclick="setIdProofAccept(' . $row['user_id'] . ', 1)" > ' . lang('accept') . '&nbsp;&nbsp';
+                $data .= '<input type="radio" name="bank_proof_status"  id="bank_reject" value="reject" onclick="setIdProofReject(' . $row['user_id'] . ', 1)"  > ' . lang('reject') . '';
+            }
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group" id="bank_reject_div" style="display:none;
+            ">';
+            $data .= '<div class="input-group">';
+
+            $data .= '<textarea type="text" rows="4" cols="50" name="bank_reason" id="bank_reason"></textarea>';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '</div>';
+
+            $data .= '<div class="col-md-6">';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('id_name') . '</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="id_name" id="id_name" value="' . $row['id_name'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label">' . lang('id_number') . '</label>';
+            $data .= '<div class="input-group">';
+            $data .= '<input type="text" name="id_name" id="id_name" value="' . $row['id_number'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+            $data .= '<div class="form-group">';
+            $data .= '<label class="control-label>' . lang('id_proof') . '</label>';
+            $data .= '<div class = "input-group">';
+            $data .= '<img width = "150" height = "150" src = "' . BASE_PATH . 'assets/images/id_proof/' . $row['id_proof'] . '">';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class = "form-group">';
+            $data .= '<div class = "input-group">';
+            if ($id_proof_status == 'accept') {
+                $data .= '<input type = "hidden" name = "id_proof_status"  id = "id_accept" value = "accept" onclick = "setIdProofAccept(' . $row['user_id'] . ',2)" ><span style="color:green" class="fa fa-check"> ' . lang('accepted') . '</span>&nbsp;&nbsp;';
+                $data .= '<input type = "radio"  name = "id_proof_status"  id = "id_reject" value = "reject" onclick = "setIdProofReject(' . $row['user_id'] . ',2)" > ' . lang('reject') . '';
+            } else if ($id_proof_status == 'reject') {
+                $data .= '<input type = "radio" name = "id_proof_status"  id = "id_accept" value = "accept" onclick = "setIdProofAccept(' . $row['user_id'] . ',2)" > ' . lang('accept') . '</span>&nbsp;&nbsp;';
+                $data .= '<input type = "hidden"  name = "id_proof_status"  id = "id_reject" value = "reject" onclick = "setIdProofReject(' . $row['user_id'] . ',2)" ><span style="color:red" class="fa fa-trash">  ' . lang('rejected') . '</span>';
+            } else {
+                $data .= '<input type = "radio"  name = "id_proof_status"  id = "id_accept" value = "accept" onclick = "setIdProofAccept(' . $row['user_id'] . ',2)" >' . lang('accept') . '</span>&nbsp;&nbsp;';
+                $data .= '<input type = "radio" name = "id_proof_status"  id = "id_reject" value = "reject" onclick = "setIdProofReject(' . $row['user_id'] . ',2)" > ' . lang('reject') . '';
+            }
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '<div class = "form-group" id = "id_reject_div" style = "display:none;">';
+            $data .= '<div class = "input-group">';
+
+            $data .= '<textarea type = "text" rows = "4" cols = "50" name = "id_reason" id = "id_reason" ></textarea>';
+
+            $data .= '</div>';
+            $data .= '</div>';
+
+            $data .= '</div>';
+        }
+        return $data;
+    }
+
+    function isUserExits($user_id) {
+        $flag = false;
+        $result = $this->db->select("count(*) ")
+                ->from("kyc_details")
+                ->where('user_id', $user_id)
+                ->count_all_results();
+        if ($result > 0) {
+            $flag = true;
+        }
+        return $flag;
+    }
+
+    function updateKycStatus($userid, $status, $type) {
+        $result = $this->db->set($type . '_proof_status', $status)->where('user_id', $userid)->update('kyc_details');
+        return $result;
+    }
+
+    function updateRejectKycDetails($userid, $status, $type, $data) {
+        $result = $this->db->set($type . '_proof_status', $status)->set($type . '_proof_cancel_reason',$data)->where('user_id', $userid)->update('kyc_details');
+
+        return $result;
+    }
+
 }
